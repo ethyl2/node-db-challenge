1b93dfa204304e39fa0651ae893a770f3bec54fa
Heather Nuffer <zzzuzz@gmail.com>
2020-01-24 09:10:36 -0700

    Added server

---
+++ api/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+require('dotenv').config();
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+
+//Put router stuff here
+
+const server = express();
+server.use(express.json());
+server.use(helmet());
+server.use(morgan('tiny'));
+server.use(cors());
+
+const port =  process.env.PORT || 9000;
+
+server.get('/', (req, res) => res.send(`API up and running on port ${port}`))
+
+module.exports = server;
