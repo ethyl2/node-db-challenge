c2dc957ab9704c7d7b616b21b2b7c5517f18b9c5
Heather Nuffer <zzzuzz@gmail.com>
2020-01-24 09:55:08 -0700

    Added GET, POST for resources

--- resources/resources-model.js
+++ resources/resources-model.js
@@ -1,5 +1,7 @@
 module.exports = {
-    getResources
+    getResources,
+    addResource,
+    getResource
 }
 
 const db = require('../data/db-config.js');
@@ -7,3 +9,17 @@ const db = require('../data/db-config.js');
 function getResources() {
     return db('resources');
 }
+
+function addResource(newResource) {
+    return db('resources').insert(newResource)
+        .then(ids => {
+            return getResource(ids[0]);
+        });
+}
+
+function getResource(resource_id) {
+    return db('resources')
+        .where({id: resource_id})
+        .first();
+}
+
--- resources/resources-router.js
+++ resources/resources-router.js
@@ -3,8 +3,37 @@ const Resources = require('./resources-model.js');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Connected to the resources router');
+    Resources.getResources()
+        .then(response => {
+            res.status(200).json(response);
+        })
+        .catch(err => {
+            res.status(500).json({error: err, message: 'Unable to retrieve resources'});
+        });
 }
 );
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    Resources.getResource(id)
+        .then(response => {
+            res.status(200).json(response);
+        })
+        .catch(err => {
+            res.status(500).json({error: err, message: `Unable to retrieve resource of id ${id}`});
+        });
+});
+
+router.post('/', (req, res) => {
+    const resourceData = req.body;
+
+    Resources.addResource(resourceData)
+        .then(resource => {
+            res.status(201).json(resource);
+        })
+        .catch (err => {
+            res.status(500).json({ message: 'Failed to create new resource' });
+        }); 
+});
 
 module.exports = router;
